14b34ec7624c53df64db61000de5017807f0fdb5
Nikita Mitroshin <nikitamitroshin@gmail.com>
2018-01-13 15:00:05 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,8 @@ import org.opencv.objdetect.Objdetect;
 
 public class Main {
 
-    private static final String FILE_NAME = "resources/images/test/dasha.jpg";
-    private static final String RESULT_FILE_NAME = "resources/images/result/dasha.jpg";
+    private static final String FILE_NAME = "resources/images/test/white_face.jpg";
+    private static final String RESULT_FILE_NAME = "resources/images/result/result%s.jpg";
 
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -34,18 +34,19 @@ public class Main {
 
         final CascadeClassifier faceCascade = new CascadeClassifier();
         faceCascade.load("resources/haarcascades/haarcascade_frontalface_alt.xml");
-
         // detect faces
         faceCascade.detectMultiScale(grayFrame, faces, 1.1, 2, Objdetect.CASCADE_SCALE_IMAGE,
                 new Size(absoluteFaceSize, absoluteFaceSize), new Size());
 
         // each rectangle in faces is a face: draw them!
         Rect[] facesArray = faces.toArray();
-        for (Rect aFacesArray : facesArray) {
-            Imgproc.rectangle(radImage, aFacesArray.tl(), aFacesArray.br(), new Scalar(0, 255, 0), 3);
+        for (int i = 0; i < facesArray.length; i++) {
+//        for (Rect aFacesArray : facesArray) {
+//            Imgproc.rectangle(radImage, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0), 3);
+            Mat image_roi = new Mat(radImage,facesArray[i]);
+            Imgcodecs.imwrite(String.format(RESULT_FILE_NAME, System.currentTimeMillis()), image_roi);
         }
-//        Mat image_roi = new Mat(radImage,facesArray[0]);
-//        Imgcodecs.imwrite(RESULT_FILE_NAME, image_roi);
-        Imgcodecs.imwrite(RESULT_FILE_NAME, radImage);
+
+//        Imgcodecs.imwrite(RESULT_FILE_NAME, radImage);
     }
 }
